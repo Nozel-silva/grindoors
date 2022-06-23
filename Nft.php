4cf7e443c0e92966b40ed8ca9de9919cf6a0f620
Nuel Agafie <70712577+Nozel-silva@users.noreply.github.com>
2022-06-23 23:02:42 +0100

    Add files via upload

---
+++ Nft.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" type="image/jpeg" href="img/IMG_20210702_143955.jpg">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <script src='http://use.edgefonts.net/creepster.js'></script>
+
+  <title>Grindoors</title>
+</head>
+<style type="text/css">
+  nav{
+    color: ghostwhite;
+    opacity: 1;
+  }
+  body{ 
+margin-top: 5%;
+margin-left: 5%;
+margin-right: 5%;
+background-color: black;
+  background-size: cover;
+    color: whitesmoke;
+      background-position: relative;
+
+       font-family: courier;
+       height:auto;
+       
+     }
+
+h2{
+margin-left: 5%;
+text-decoration: underline;
+}
+#info{
+  font-size: 24px;
+  margin-top: 9px;
+  color: magenta;
+  padding: 10px;
+}
+b{
+  margin-top: 12px;
+}
+hr{
+  color: black;
+}
+
+@keyframes colorchange{0%{color: gold; } 25%{color: goldenyellow;} 50%{color: red;}100%{color:white;}}
+
+</style>
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+
+  <div class="nav">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">Grindooors</a>
+    </div>
+    <div>
+      <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav" >
+          <li><a href="index.php">Home</a></li>
+         <li><a href="#section42">login</a></li>
+          
+        </ul>
+      </div>
+    </div>
+    
+  </div>
+</nav>    
+<marquee behavior="scroll" direction="left">
+ <b style="font-size: 30px;" > Ensure you check the servers you join and get whitelisted in</b>
+</marquee>
+<div id="rbd">
+<h2>Ongoing</h2>  
+</div>
+<div id="info">
+  <b>Name:</b><br>
+  <b>Discord url:</b><br>
+  <b>Launch Date:</b><br>
+  <b>Mint Date:</b><br>
+  <b>Difficulty:</b><br>
+  <b>Joined <input type="checkbox" name=""></b><br>
+  <b>Whitelisted <input type="checkbox" name=""></b><br>
+  <button class="btn btn-primary">Save</button>
+</div>
+<hr>
+<div id="info">
+  <b>Name:</b><br>
+  <b>Discord url:</b><br>
+  <b>Launch Date:</b><br>
+  <b>Mint Date:</b><br>
+  <b>Difficulty:</b><br>
+  <b>Joined <input type="checkbox" name=""></b><br>
+  <b>Whitelisted <input type="checkbox" name=""></b><br>
+  <button class="btn btn-primary">Save</button>
+</div>
+<hr>
+</body>
+</html>
+
+
+
---
+++ index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" type="image/jpeg" href="img/IMG_20210702_143955.jpg">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <script src='http://use.edgefonts.net/creepster.js'></script>
+
+	<title>Grindoors</title>
+</head>
+<style type="text/css">
+  nav{
+    color: ghostwhite;
+    opacity: 1;
+  }
+	body{ 
+margin-top: 5%;
+margin-left: 5%;
+margin-right: 5%;
+  
+ background-color: black;
+  background-size: cover;
+    color: magenta;
+      background-position: relative;
+
+       font-family: courier;
+       height:600px;
+       
+     }
+
+h2{
+  margin-top: 22px;
+margin-left: 5%;
+text-decoration: underline;
+
+}
+#info{
+  margin-top: 22px;
+  
+  padding: 10px;
+}
+b{
+  margin-top: 12px;
+  font-size: 30px;
+}
+hr{
+  color: ;
+}
+center{
+  margin-top: 100px;
+}
+a{
+  color: whitesmoke;
+}
+a:hover{
+  color: blue;
+}
+marquee i{
+
+ animation-name: colorchange;
+  animation-duration: 5s;
+   animation-iteration-count: infinite;
+}
+
+@keyframes colorchange{0%{color: yellow; } 25%{color: deepskyblue;} 50%{color: red;}100%{color:springgreen;}}
+.image{
+  float: right;
+}
+center{
+  clear: both;
+}
+</style>
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+
+  <div class="nav">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">Grindooors</a>
+    </div>
+    <div>
+      <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav" >
+          <li><a href="#section42">login</a></li>
+         
+          
+        </ul>
+      </div>
+    </div>
+    
+  </div>
+</nav>    
+<marquee behavior="scroll" direction="left">
+ <i style="font-size: 30px; " > Ensure you are visiting Grindoors.com</i>
+</marquee>
+<div id="rbd">
+<h2>Getting started?</h2>  
+</div>
+<div id="info">
+  <ul>
+    <li><a href="#">With Nft</a></li><br>
+    <li><a href="#">Airdrops</a></li>
+    <div class="image"><marquee direction="down" behavior="scroll"><img src="image/images (2) (1).jpeg" width="370px;" height="270px;"></marquee></div>
+  </ul>
+</div>
+<div id="ava">
+  <h2>Available</h2>
+  <ul>
+    <li><a href="Nft.php">Nft</a></li>
+    <li><a href="#">Airdrops</a></li>
+  </ul>
+</div>
+
+<center>
+<div class="social" style="margin-bottom: 20px;">
+      
+      <a href="https://www.facebook.com/socialprintstudio" style="margin: 0 10px;text-decoration:none;">
+        <img src="https://s3.amazonaws.com/socialps-assets/join/fb.png?a=2" style="max-height: 25px;">
+      </a>
+      
+      <a href="https://twitter.com/socialps" style="margin: 0 10px;text-decoration:none;">
+        <img src="https://s3.amazonaws.com/socialps-assets/join/twitter.png?a=2" style="max-height: 25px;">
+      </a>
+      
+      <a href="https://www.youtube.com/user/SocialPs" style="margin: 0 10px;">
+        <img src="https://s3.amazonaws.com/socialps-assets/join/youtube.png?a=2" style="max-height: 25px;">
+      </a>
+    </div>
+</center>
+
+</body>
+</html>
+
+
+
